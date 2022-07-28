eb27bf9e709f4647071331365c07d95698906085
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-28 11:54:13 -0700

    Initial Commit

---
+++ 20220728-DmitriiKim-NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20220728-DmitriiKim-NYCSchools
+//
+//  Created by Mitya Kim on 7/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
